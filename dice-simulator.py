78c29aaa2e04a5a569bfa1b27e3edd3eb4dbf39e
Candousek <candousek@email.cz>
2022-07-11 00:09:47 +0200

    Create dice-simulator.py

---
+++ dice-simulator.py
@@ -0,0 +1,69 @@
+from random import randint
+
+def getPercentage(data, number):
+    result = data[number] / data["total"] * 100
+    return f"{result:.2f}%"
+
+data = {
+    "total": 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+}
+
+SIDES = {
+    1: """
+    [     ]
+    [  X  ]
+    [     ]
+    """,
+    2: """
+    [X    ]
+    [     ]
+    [    X]
+    """,
+    3: """
+    [X    ]
+    [  X  ]
+    [    X]
+    """,
+    4: """
+    [X   X]
+    [     ]
+    [X   X]
+    """,
+    5: """
+    [X   X]
+    [  X  ]
+    [X   X]
+    """,
+    6: """
+    [X   X]
+    [X   X]
+    [X   X]
+    """
+}
+
+while True:
+    
+    decision = input("""
+    [B] Stop Rolling
+    [S] Show statistics 
+    [Enter] Roll
+    >> Your choice: """).lower()
+
+    if decision == "b":
+        break
+    elif decision == "s":
+        result = "\n>> Showing statistics:\n"
+        for key in data:
+            result += f"You rolled {key}: {data[key]} time{'s' if data[key] != 1 else ''} ({getPercentage(data, key)})\n"
+        print(result)
+    else:
+        roll = randint(1, 6)
+        data[roll] += 1
+        data["total"] += 1
+        print(f"{SIDES[roll]}Your rolled {roll}!")
